85d71fe9837b56f9ca14379787077623fbcf73b9
rsz <rhenansz@hotmail.com>
2022-12-11 11:02:43 -0300

    fix: general fixing

--- Program.cs
+++ Program.cs
@@ -14,12 +14,13 @@ builder.Services.AddScoped<IContext, Context>();
 #endregion
 
 #region Repositories
-builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 #endregion
 
 #region Services
 builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IPostService, PostService>();
 #endregion
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
