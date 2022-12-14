6d93b2088982d18ca3915557250109e284f8bb2f
rsz <rhenansz@hotmail.com>
2022-12-14 03:53:42 -0300

    feat: implemented deploy settings

--- Tryitter.app/Program.cs
+++ Tryitter.app/Program.cs
@@ -90,7 +90,7 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
